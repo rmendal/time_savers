c3ebf80118a0ad9021e14af1516f1324f6ebdd06
Robert <rmendal@gmail.com>
2018-08-21 08:16:51 -0400

    removing test yaml files

--- yaml_maker.py
+++ yaml_maker.py
@@ -53,7 +53,7 @@ def git(file_list):
 
     subprocess.call(f"git commit -a -m '{message}'", shell=True)
     subprocess.call("git push", shell=True)
-    subprocess.call("git request-pull", shell=True)
+    subprocess.call("git request-pull -p", shell=True)
     return None
 
 
