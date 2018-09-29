47ef638d91045921a00cfcfa91050e8a61896621
Robert <rmendal@gmail.com>
2018-09-29 11:17:26 -0400

    Added dig action to hostname.

--- get_virt_list.py
+++ get_virt_list.py
@@ -14,9 +14,11 @@ import argparse
 
 def virt_list(host_start, host_end, site):
     """
-    First collect a sudo password. Next we ping the host to make sure it actually is alive. Not alive? Iterate to the
-    next host. Alive? SSH to it and and print out the result of virsh list --all. Since this requires ping I've added a
-    progress bar as it takes a bit to work through all of the hosts and otherwise looks stalled.
+    First collect a sudo password. Next we dig with the +short flag and if we get an IP address back we continue with
+    ping. If no IP back we skip to the next host. If we get an IP we ping the host to make sure it actually is alive.
+    Not alive? Iterate to the next host. Alive? SSH to it and and print out the result of virsh list --all.
+    Since this requires ping I've added a progress bar as it takes a bit to work through all of the hosts and otherwise
+    looks stalled.
     :param host_start: server number to start at
     :param host_end: server number to end at
     :param site: physical site of servers, e.g. tym
@@ -25,14 +27,19 @@ def virt_list(host_start, host_end, site):
     sudo_pass = getpass("Env Password: ")
     config = Config(overrides={'sudo': {'password': sudo_pass}})
     for i in tqdm(range(int(host_start), (host_end + 1)), desc="Progress"):
-        ping = str(run(f"ping -c 4 vm{i}.{site}.cudaops.com", shell=True, stdout=PIPE))
-        ping = ''.join(ping)
-        if "bn-scl-redirect.cuda-inc.com" in ping or "100% packet loss" in ping:
+        dig = str(run(f"dig +short vm{i}.{site}.cudaops.com", shell=True, stdout=PIPE))
+        dig = ''.join(dig)
+        if "stdout=b''" in dig:
             continue
         else:
-            conn = Connection(f"vm{i}.{site}.cudaops.com", config=config)
-            virsh_all = str(conn.sudo("virsh list --all", hide="stdout"))
-            print(f"vm{i}.{site}.cudaops.com\n{virsh_all}")
+            ping = str(run(f"ping -c 4 vm{i}.{site}.cudaops.com", shell=True, stdout=PIPE))
+            ping = ''.join(ping)
+            if "bn-scl-redirect.cuda-inc.com" in ping or "100% packet loss" in ping:
+                    continue
+            else:
+                conn = Connection(f"vm{i}.{site}.cudaops.com", config=config)
+                virsh_all = str(conn.sudo("virsh list --all", hide="stdout"))
+                print(f"vm{i}.{site}.cudaops.com\n{virsh_all}")
     return None
 
 
